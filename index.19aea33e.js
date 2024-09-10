a6cac63f7e26b424fcdc92e86fed02b840c77eed
Anna Agerone <olesnevych12@gmail.com>
2024-09-10 16:54:21 -0500

    make build

---
+++ index.19aea33e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelectorAll("a").forEach(function(r){r.addEventListener("click",function(c){e.src=r.href,c.preventDefault()})});
+//# sourceMappingURL=index.19aea33e.js.map
